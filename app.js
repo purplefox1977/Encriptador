e27eda151129fa72579dd06db0ababc0966eb95a
loboplateado <loboplateado1203@gmail.com>
2024-07-18 22:59:04 -0300

    1

---
+++ app.js
@@ -0,0 +1,22 @@
+/*¡
+Las "llaves" de encriptación que utilizaremos son las siguientes:
+
+La letra "e" es convertida para "enter"
+La letra "i" es convertida para "imes"
+La letra "a" es convertida para "ai"
+La letra "o" es convertida para "ober"
+La letra "u" es convertida para "ufat"
+
+Debe funcionar solo con letras minúsculas
+No deben ser utilizados letras con acentos ni caracteres especiales
+Debe ser posible convertir una palabra para la versión encriptada también devolver una palabra encriptada para su versión original.
+Por ejemplo:
+"gato" => "gaitober"
+gaitober" => "gato"
+
+inserción del texto que será encriptado o desencriptado, y el usuario debe poder escoger entre as dos opciones.
+El resultado debe ser mostrado en la pantalla.
+
+Un botón que copie el texto encriptado/desencriptado para la sección de transferencia, o sea que tenga la misma funcionalidad del ctrl+C o de la opción "copiar" del menú de las aplicaciones.
+Tenemos un periodo de tiempo de cuatro semanas para desarrollar el proyecto y vamos a trabajar con el sistema ágil de desarrollo, utilizando el Trello de la siguiente forma:
+
